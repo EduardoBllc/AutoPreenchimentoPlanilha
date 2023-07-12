346cee92ef7ffc184a00bfe1984b169f7eff019c
Eduardo <eduardo.sballico@gmail.com>
2023-07-11 23:59:07 -0300

    Ajustando e adicionando alguns tratamentos

--- main.py
+++ main.py
@@ -3,25 +3,48 @@ import random
 
 # Abrindo e lendo o arquivo.txt
 with open("palavras.txt", "r", encoding="utf-8-sig") as arquivo:
-    # Capturando as palavras em uma lista sem o "\n" no final
-    texto: list[str] = [palavra[:-1] for palavra in arquivo.readlines()]
+    # Recebendo as palavras em uma lista sem o "\n" no final e separando caso tenha mais de 1 palavra por linha
+    lista_palavras: list[list[str] | str] = [palavra[:-1].split(" ") if palavra.count(" ") else palavra[:-1]
+                                             for palavra in arquivo.readlines()]
+
+# Certificando que todas as palavras estão separadas ou em listas de palavras
+if [','] in lista_palavras:
+    for item in lista_palavras:
+        item.split(',')
+if [';'] in lista_palavras:
+    for item in lista_palavras:
+        item.split(';')
+
+terminacoes: list[str] = ['ão', 'om']
+
+# Para cada item da lista_palavras(que pode ter tanto strings quanto listas por conta do split)
+for item in lista_palavras:
+    # Se o item for uma lista
+    if type(item) == list:
+        # Para cada palavra dessa lista
+        for palavra in item:
+            # Se a palavra tiver a terminação indicada e a não estiver repetida, adiciona a palavra na lista_palavras
+            if palavra[-len(terminacoes[0]):] in terminacoes and lista_palavras.count(item) < 2:
+                lista_palavras.append(palavra)
+        # Remove a lista após tratamento
+        lista_palavras.remove(item)
+    # Se não for uma lista mas não estiver de acordo com as terminações indicadas ou estiver repetida, remove o item
+    elif item[-len(terminacoes[0]):] not in terminacoes or lista_palavras.count(item) > 1:
+        lista_palavras.remove(item)
 
 # Embaralhando as palavras
-random.shuffle(texto)
+random.shuffle(lista_palavras)
 
 # Declarando a varíavel da tabela, que irá receber um dict para cada linha
-tabela = []
+tabela: list[dict[int:str]] = []
 
-while len(texto) >= 6:
-    # Criando um dicionário com as primeiras 6 linhas atuais e inserindo na tabela
-    palavras = texto[:6]
-    tabela.append({1: palavras[0], 2: palavras[1], 3: palavras[2],
-                   4: palavras[3], 5: palavras[4], 6: palavras[5]})
-    # Apagando as primeiras 6 palavras
-    texto = texto[6:]
+# Limitando em 6 para não gerar linhas incompletas
+while len(lista_palavras) >= 5:
 
-# Transformando a tabela em um DataFrame
-tabela = pd.DataFrame(data=tabela)
+    # Criando um dicionário com as primeiras 5 linhas atuais e inserindo na tabela
+    tabela.append({lista_palavras.index(palavra): palavra for palavra in lista_palavras[:5]})
+    # Apagando as primeiras 5 palavras
+    lista_palavras = lista_palavras[5:]
 
 # Gerando o arquivo em excel da tabela sem o cabeçalho e a coluna de índice
-tabela.to_excel("tabela_de_palavras.xlsx", header=False, index=False)
+pd.DataFrame(data=tabela).to_excel("tabela_de_palavras.xlsx", header=False, index=False)
